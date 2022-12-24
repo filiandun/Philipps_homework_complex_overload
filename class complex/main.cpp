3eec1fb95ebd9eb806834cb610fb979d95e6f475
filian <filiandun@gmail.com>
2022-12-24 19:23:53 +0300

    main: ready

--- class complex/main.cpp
+++ class complex/main.cpp
@@ -1,9 +1,19 @@
 ﻿#include <iostream>
+#include "complex.h"
 
 int main()
 {
     setlocale(LC_ALL, "rus");
 
+	Complex first_complex_number(2, 5);
+	Complex second_complex_number(3, 4);
 
+	std::cout << "Первое комплексное число: " << first_complex_number;
+	std::cout << "Второе комплексное число: " << second_complex_number << std::endl;
+
+	std::cout << "Сложение: " << first_complex_number + second_complex_number;
+	std::cout << "Вычитание: " << first_complex_number - second_complex_number;
+	std::cout << "Умножение: " << first_complex_number * second_complex_number;
+	std::cout << "Деление: " << first_complex_number / second_complex_number;
 }
 
